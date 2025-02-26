d9a9d171b5a8b3dc26c22dec2c2c5dd2fcb7b58e
lavaniarajeev001 <rajeev.lavania@yahoo.co.in>
2025-02-26 10:35:55 +0530

    first commit

---
+++ template.py
@@ -0,0 +1,68 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s]:%(message)s"
+)
+
+project_name="cnnClassifier"
+
+list_of_file=[
+    "github/workflows/.gitkeep"
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/pipeline/stage_01_data_ingestion.py",
+    f"src/{project_name}/pipeline/stage_02_data_validation.py",
+    f"src/{project_name}/pipeline/stage_03_data_transformation.py",
+    f"src/{project_name}/pipeline/stage_04_model_trainer.py",
+    f"src/{project_name}/pipeline/stage_05_model_evaluation.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/components/data_validation.py",
+    f"src/{project_name}/components/model_evaluation.py",
+    "research/1.data_ingestion.ipynb",
+    "research/2.data_validation.ipynb",
+    "research/3.data_transformation.ipynb",
+    "research/4.model_trainer.ipynb",
+    "research/5.model_evaluation.ipynb",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    "dvc.yaml"
+]
+
+for filepath in list_of_file:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating {filedir} for the file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+        logging.info(f"creating empty file {filename}")
+
+    else:
+        logging.info(f"{filename} is already exists")
